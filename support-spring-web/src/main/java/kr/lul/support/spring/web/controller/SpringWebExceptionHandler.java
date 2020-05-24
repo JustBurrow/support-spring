305b6e22399cbfeffb2ccb83baec5abba2f8dfbd
Just Burrow <just.burrow@lul.kr>
2020-05-24 13:00:29 +0900

    ADD : 에러 핸들러 타입 추가.

--- support-spring-web/src/main/java/kr/lul/support/spring/web/controller/SpringWebExceptionHandler.java
+++ support-spring-web/src/main/java/kr/lul/support/spring/web/controller/SpringWebExceptionHandler.java
@@ -3,9 +3,11 @@ package kr.lul.support.spring.web.controller;
 import org.slf4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.client.HttpClientErrorException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -23,6 +25,7 @@ public class SpringWebExceptionHandler {
 
   public static final String VIEW_GROUP = "error";
   public static final String VIEW_400 = VIEW_GROUP + "/client/400";
+  public static final String VIEW_404 = VIEW_GROUP + "/client/404";
 
   @ExceptionHandler(ServletException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
@@ -37,5 +40,32 @@ public class SpringWebExceptionHandler {
     if (log.isTraceEnabled())
       log.trace("#servletException result : template={}, model={}", VIEW_400, model);
     return VIEW_400;
+  }
+
+  @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public String methodNotSupportedException(HttpRequestMethodNotSupportedException e, HttpServletRequest request, Model model) {
+    if (log.isTraceEnabled())
+      log.trace("#methodNotSupportedException args : e=" + e.getMessage() + ", request={}, model={}", request, model);
+
+    return VIEW_404;
+  }
+
+  @ExceptionHandler(HttpClientErrorException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public String badRequest(HttpClientErrorException e, HttpServletRequest request, Model model) {
+    log.info("#badRequest args : e=" + e.getMessage() + ", request={}, model={}", request, model);
+
+    return VIEW_400;
+  }
+
+  @ExceptionHandler({HttpClientErrorException.NotFound.class})
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public String notFound(HttpClientErrorException.NotFound e, HttpServletRequest request, Model model) {
+    if (log.isDebugEnabled())
+      log.debug("#notFound e=" + e.getMessage() + ", request={} {}, model={}",
+          request.getMethod(), request.getRequestURL(), model);
+
+    return VIEW_404;
   }
 }
